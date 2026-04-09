29776fc3b5fdd1a8dbeed155da69fe144c57eafa
unknown <andylee0311a@google.com>
2026-04-09 15:49:34 +0800

    Initial commit: 訂閱管家 App

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // 啟用深色模式支援
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
